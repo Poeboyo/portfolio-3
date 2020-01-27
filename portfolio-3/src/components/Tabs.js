675c73e7ec53f76856ac583dd66796ea58cce186
Jeffrey Poe <jeffreypoe@jeffreys-mbp-2.lan>
2020-01-27 17:40:25 -0500

    Added CSS and materialize

--- portfolio-3/src/components/Tabs.js
+++ portfolio-3/src/components/Tabs.js
@@ -4,10 +4,10 @@ class Tabs extends Component {
   render() {
     return (
       <div class="row tab-management">
-        <div class="col s12">
+        <div class="col s12 white">
           <ul class="tabs">
             <li class="tab col s4">
-              <a href="#about-me">About Me / Resume </a>
+              <a href="#about-me">About Me / Resume</a>
             </li>
             <li class="tab col s4">
               <a class="active" href="#portfolio">
@@ -20,13 +20,17 @@ class Tabs extends Component {
           </ul>
         </div>
         <div id="about-me" class="col s12">
-          Test 1
+          <div class="row">
+            <div class="col s4 sides"></div>
+            <div class="col s4"></div>
+            <div class="col s4 sides"></div>
+          </div>
         </div>
         <div id="portfolio" class="col s12">
-          Test 2
+          Portfolio
         </div>
         <div id="contact-me" class="col s12">
-          Test 3
+          Contact Me
         </div>
       </div>
     );
