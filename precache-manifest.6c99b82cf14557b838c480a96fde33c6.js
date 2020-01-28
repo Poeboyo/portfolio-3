0ea5ac9b87b3ea3b6828c3ac505ff6eeaed9ee2b
Jeffrey Poe <jeffreypoe@jeffreys-mbp-2.lan>
2020-01-27 22:02:57 -0500

    Updates

---
+++ precache-manifest.6c99b82cf14557b838c480a96fde33c6.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b0aeceaedf1510520f2ce4cf54e16a3",
+    "url": "/portfolio-3/index.html"
+  },
+  {
+    "revision": "18395ecded74f61ff591",
+    "url": "/portfolio-3/static/css/main.7a5d5e7b.chunk.css"
+  },
+  {
+    "revision": "9904aa354271f684cff1",
+    "url": "/portfolio-3/static/js/2.84b3118f.chunk.js"
+  },
+  {
+    "revision": "202fb2c159b07ffdfc5ce677a1cf946e",
+    "url": "/portfolio-3/static/js/2.84b3118f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18395ecded74f61ff591",
+    "url": "/portfolio-3/static/js/main.ead14f50.chunk.js"
+  },
+  {
+    "revision": "e6575e430c9820bf4195",
+    "url": "/portfolio-3/static/js/runtime-main.cf6a5abc.js"
+  },
+  {
+    "revision": "bdf1a4efad1430da43eba53044e16921",
+    "url": "/portfolio-3/static/media/bamazon.bdf1a4ef.png"
+  },
+  {
+    "revision": "90e9a0704296f22711e257417fd8da44",
+    "url": "/portfolio-3/static/media/burger.90e9a070.png"
+  },
+  {
+    "revision": "0ee26cdb23789eea5eaa290964d918cf",
+    "url": "/portfolio-3/static/media/codewars.0ee26cdb.png"
+  },
+  {
+    "revision": "dc077e0c0600dbd3fab32eb05d014a64",
+    "url": "/portfolio-3/static/media/csk.dc077e0c.png"
+  },
+  {
+    "revision": "e9c47db7a496a046f8e6b2f096ab1405",
+    "url": "/portfolio-3/static/media/css-logo.e9c47db7.png"
+  },
+  {
+    "revision": "4625283162752daf2955ebb616d55782",
+    "url": "/portfolio-3/static/media/gif.46252831.png"
+  },
+  {
+    "revision": "ecc4cf252a967f043b7cc229560486ac",
+    "url": "/portfolio-3/static/media/github-logo.ecc4cf25.png"
+  },
+  {
+    "revision": "8889ecd2f2e57d17533b8a60567d6cbc",
+    "url": "/portfolio-3/static/media/headshotcopy.8889ecd2.jpeg"
+  },
+  {
+    "revision": "61fa6a18df7aeb1a4b3a3dfe091818db",
+    "url": "/portfolio-3/static/media/hollow-knight.61fa6a18.png"
+  },
+  {
+    "revision": "db62a803d10074346a48f7ee04307a31",
+    "url": "/portfolio-3/static/media/liri.db62a803.png"
+  },
+  {
+    "revision": "d712f618e35dec85e45ea1a32d959d7c",
+    "url": "/portfolio-3/static/media/majora.d712f618.png"
+  },
+  {
+    "revision": "abcd506c4933ddfc55209aa51f83cd61",
+    "url": "/portfolio-3/static/media/mongodb-logo.abcd506c.png"
+  },
+  {
+    "revision": "3d61ef45e5a8e024c2097c5b8ee99932",
+    "url": "/portfolio-3/static/media/node-logo.3d61ef45.png"
+  },
+  {
+    "revision": "5e8511db78809ebe710d7f42eca3a335",
+    "url": "/portfolio-3/static/media/psychic.5e8511db.jpg"
+  },
+  {
+    "revision": "ce660fdc70a9f22178cda86b463d35dc",
+    "url": "/portfolio-3/static/media/react-logo.ce660fdc.png"
+  },
+  {
+    "revision": "d96a3d08e2a76ef00ec8e529838e54bb",
+    "url": "/portfolio-3/static/media/sequelize-logo.d96a3d08.png"
+  },
+  {
+    "revision": "9b95b3788ee77cd5c505b3cd61e254d6",
+    "url": "/portfolio-3/static/media/train.9b95b378.png"
+  },
+  {
+    "revision": "dd79ebd7c574b01761b0ad60271148f2",
+    "url": "/portfolio-3/static/media/whether.dd79ebd7.png"
+  }
+]);
