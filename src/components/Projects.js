c62ea1ae1427ba3988f60952613d0af6368a3e08
Jeffrey Poe <jeffreypoe@jeffreys-mbp-2.lan>
2020-01-27 22:46:33 -0500

    Made changes to site

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -463,21 +463,16 @@ class Projects extends Component {
                       </li>
                     </ul>
                   </p>
-                  <p>
-                    <a href="https://github.com/wedodge89/cozy-couch">
-                      <img
-                        class="logo github-link"
-                        src={gitlink}
-                        alt="Github Link"
-                      />
-                    </a>
-                    <a
-                      href="https://shelfy-shelf.herokuapp.com/"
-                      target="_blank"
-                    >
-                      <p>Live Site</p>
-                    </a>
-                  </p>
+                  <a href="https://github.com/wedodge89/cozy-couch">
+                    <img
+                      class="logo github-link"
+                      src={gitlink}
+                      alt="Github Link"
+                    />
+                  </a>
+                  <a href="https://shelfy-shelf.herokuapp.com/" target="_blank">
+                    <p>Live Site</p>
+                  </a>
                 </div>
               </div>
             </div>
